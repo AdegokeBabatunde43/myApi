d46323fd6bfeb580e49b6656e825129e1e87e52e
AdegokeBabatunde43 <101894263+AdegokeBabatunde43@users.noreply.github.com>
2022-07-27 21:30:14 +0100

    first commit(deploy): Setup deploy to heroku

--- adegoke/settings.py
+++ adegoke/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,6 +133,8 @@ CORS_ALLOWED_ORIGINS = [
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+django_heroku.settings(locals())
 
 STATIC = os.path.join(BASE_DIR, 'staticfiles')
 
